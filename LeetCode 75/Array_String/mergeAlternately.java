dffb8a1a00a2595a6f55a86aabb02ec35209112e
SantoshMule2004 <mulesantosh489@gmail.com>
2025-09-04 18:58:21 +0530

    Added LeetCode Problem 1768

---
+++ LeetCode 75/Array_String/mergeAlternately.java
@@ -0,0 +1,20 @@
+class mergeAlternately {
+    public String mergeStringAlternately(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+
+        StringBuilder ans = new StringBuilder("");
+        int i = 0, j = 0;
+        while(i < m || j < n) {
+            if(i<m) {
+                ans.append(word1.charAt(i));
+                i++;
+            }
+            if(j<n) {
+                ans.append(word2.charAt(j));
+                j++;
+            }
+        }
+        return ans.toString();
+    }
+}
