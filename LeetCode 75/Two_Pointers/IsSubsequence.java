ed9ab33392f39008c9a1fe8d576ec7c1c1aeff8e
SantoshMule2004 <mulesantosh489@gmail.com>
2025-09-14 11:11:18 +0530

    Added LeetCode Problem 392

---
+++ LeetCode 75/Two_Pointers/IsSubsequence.java
@@ -0,0 +1,21 @@
+class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        if(s.isEmpty()) return true;
+
+        int sSz = s.length();
+        int tSz = t.length();
+        int i=0, j=0;
+
+        while(i < tSz){
+            if(s.charAt(j) == t.charAt(i)){
+                if(j == sSz - 1) return true;
+
+                i++;
+                j++;
+            }else{
+                i++;
+            }
+        }
+        return false;
+    }
+}
