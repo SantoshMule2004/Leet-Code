88a30fdce9b720c20bdc0c6f8e5ecf49f848a88d
SantoshMule2004 <mulesantosh489@gmail.com>
2025-09-17 08:27:17 +0530

    Added LeetCode Problem 643

---
+++ LeetCode 75/Sliding_window/FindMaxAverage.java
@@ -0,0 +1,16 @@
+class FindMaxAverage {
+    public double findMaxAverage(int[] nums, int k) {
+        double sum = 0;
+        double maxAvg = 0;
+        for(int i=0; i<k; i++) {
+            sum += nums[i];
+        }
+        maxAvg = sum / k;
+
+        for(int i=k; i<nums.length; i++) {
+            sum += nums[i] - nums[i - k];
+            maxAvg = Math.max(maxAvg, sum / k);
+        }
+        return maxAvg;
+    }
+}
