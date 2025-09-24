81fcf9d9314f1214365c52cfbf340b366506fbe6
SantoshMule2004 <mulesantosh489@gmail.com>
2025-09-24 21:11:05 +0530

    Added LeetCode SQL Problem 1207

---
+++ LeetCode 75/HashMap_Set/UniqueOccurrences.java
@@ -0,0 +1,18 @@
+public class UniqueOccurrences {
+    
+}
+class UniqueOccurrences {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> hMap = new HashMap<>();
+        Set<Integer> hSet = new HashSet<>();
+
+        for(int n : arr)
+            hMap.put(n, hMap.getOrDefault(n, 0)+1);
+
+        for(int key : hMap.keySet()) {
+            if(!hSet.add(hMap.get(key)))
+                return false;
+        }
+        return true;
+    }
+}
