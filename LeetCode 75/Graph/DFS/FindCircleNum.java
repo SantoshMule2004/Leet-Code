edab6fc4cd728913e0289dfa0aec9d4a8fb26122
SantoshMule2004 <mulesantosh489@gmail.com>
2025-10-17 10:51:49 +0530

    Added LeetCode Problem 547

---
+++ LeetCode 75/Graph/DFS/FindCircleNum.java
@@ -0,0 +1,23 @@
+class FindCircleNum {
+    public int findCircleNum(int[][] isConnected) {
+        boolean[] visited = new boolean[isConnected.length];
+        int provinces = 0;
+        for(int i=0; i<isConnected.length; i++) {
+            if(!visited[i]) {
+                Queue<Integer> q = new LinkedList<>();
+                q.offer(i);
+                while(!q.isEmpty()) {
+                    int currCity = q.poll();
+                    if(!visited[currCity]) {
+                        visited[currCity] = true;
+                        for(int j=0; j<isConnected.length; j++)
+                            if(isConnected[currCity][j] == 1 && !visited[j])
+                                q.offer(j);
+                    }
+                }
+                provinces++;
+            }
+        }
+        return provinces;
+    }
+}
