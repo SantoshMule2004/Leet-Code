1bad48443cf90f79181e12a8bc91927dbcdfc2dc
SantoshMule2004 <mulesantosh489@gmail.com>
2025-11-16 17:34:51 +0530

    Added leetcode problem 1318

---
+++ LeetCode 75/Bit_Manipulation/MinFlips.java
@@ -0,0 +1,24 @@
+class MinFlips {
+    public int minFlips(int a, int b, int c) {
+        
+        int reqFlips = 0;
+        while(a > 0 || b > 0 || c > 0) {
+            int bitA = a & 1;
+            int bitB = b & 1;
+            int bitC = c & 1;
+
+            if(bitC == 0) {
+                reqFlips += (bitA + bitB);
+            }
+            else {
+                if(bitA == 0 && bitB == 0)
+                    reqFlips += 1; 
+            }
+
+            a >>= 1;
+            b >>= 1;
+            c >>= 1;
+        }
+        return reqFlips;
+    }
+}
