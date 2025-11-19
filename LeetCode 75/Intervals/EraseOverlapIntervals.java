3c32aef408b8cc7185ca55942e3b87fd618b3138
SantoshMule2004 <mulesantosh489@gmail.com>
2025-11-19 22:04:36 +0530

    Added leetcode problem 435

---
+++ LeetCode 75/Intervals/EraseOverlapIntervals.java
@@ -0,0 +1,15 @@
+class EraseOverlapIntervals {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        int prevEnd = intervals[0][1];
+        int cnt = 0;
+
+        for(int i=1; i<intervals.length; i++) {
+            if(intervals[i][0] < prevEnd)
+                cnt++;
+            else
+                prevEnd = intervals[i][1];
+        }
+        return cnt;
+    }
+}
