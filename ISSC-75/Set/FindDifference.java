9b9d271cb2732853b3f8d3377d4cdd4092ec4ae1
SantoshMule2004 <mulesantosh489@gmail.com>
2026-01-12 21:57:21 +0530

    Added Leetcode Problem 2215

---
+++ ISSC-75/Set/FindDifference.java
@@ -0,0 +1,21 @@
+public class FindDifference {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for (int n : nums1) set1.add(n);
+        for (int n : nums2) set2.add(n);
+
+        Set<Integer> diff1 = new HashSet<>(set1);
+        diff1.removeAll(set2);
+
+        Set<Integer> diff2 = new HashSet<>(set2);
+        diff2.removeAll(set1);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        ans.add(new ArrayList<>(diff1));
+        ans.add(new ArrayList<>(diff2));
+
+        return ans;
+    }
+}
