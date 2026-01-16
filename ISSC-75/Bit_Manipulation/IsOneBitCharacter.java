6795bc96a169edc1df6a1f3ae4b7d9ceb7f59a7a
SantoshMule2004 <mulesantosh489@gmail.com>
2026-01-16 23:19:02 +0530

    Added Leetcode Problem 717

---
+++ ISSC-75/Bit_Manipulation/IsOneBitCharacter.java
@@ -0,0 +1,10 @@
+class IsOneBitCharacter {
+    public boolean isOneBitCharacter(int[] bits) {
+        int i;
+        for(i=0; i<bits.length - 1; i++)
+            if(bits[i] == 1)
+                i++;
+
+        return (i == bits.length - 1);
+    }
+}
