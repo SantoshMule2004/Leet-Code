e953570d454a713924196faf6c4736af872badc1
SantoshMule2004 <mulesantosh489@gmail.com>
2026-01-17 22:50:11 +0530

    Added Leetcode Problem 26

---
+++ ISSC-75/Arrays_Strings/RemoveDuplicates.java
@@ -0,0 +1,11 @@
+class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        
+        int j = 0;
+        for(int i=0; i<nums.length - 1; i++){
+            if(nums[i] != nums[i+1])
+                nums[++j] = nums[i+1];
+        }
+        return j+1;
+    }
+}
