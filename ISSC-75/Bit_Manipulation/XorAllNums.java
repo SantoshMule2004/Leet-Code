e959582d345463a6cfee599142075397424e1f6e
SantoshMule2004 <mulesantosh489@gmail.com>
2026-01-17 22:52:11 +0530

    Added Leetcode Problem 2425

---
+++ ISSC-75/Bit_Manipulation/XorAllNums.java
@@ -0,0 +1,19 @@
+class XorAllNums {
+    public int xorAllNums(int[] nums1, int[] nums2) {
+        int sz1 = nums1.length;
+        int sz2 = nums2.length;
+
+        int sum1 = 0, sum2 = 0;
+        if(sz1%2 == 1) {
+            for(int i=0;i<sz2;i++)
+                sum1 ^= nums2[i];
+        }
+
+        if(sz2%2 == 1) {
+            for(int i=0;i<sz1;i++)
+                sum2 ^= nums1[i];
+        }
+
+        return sum1 ^ sum2;
+    }
+}
