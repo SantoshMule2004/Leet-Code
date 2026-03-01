afe1bc29b88d632eb9ef0f53689ec814b1ca1bfa
SantoshMule2004 <mulesantosh489@gmail.com>
2026-03-01 22:46:46 +0530

    Added Leetcode SQL Problem 1164

---
+++ SQL 50/AdvancedSelectandJoins/Leetcode-1164.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+SELECT product_id, IFNULL(new_price, 10) as price
+FROM Products
+WHERE (product_id, change_date) IN (
+    SELECT product_id, MAX(change_date)
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+    GROUP BY product_id
+)
+
+UNION 
+
+SELECT product_id, 10 as price
+FROM Products
+WHERE product_id NOT IN (
+    SELECT product_id
+    FROM Products
+    WHERE change_date <= '2019-08-16'
+)
